7cf1dabb33c2eeb3038d109c96889d8f9723f0a0
Casper Kessels <casperkessels@gmail.com>
2020-02-26 18:24:45 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ export default ({ children }) => {
             >
             About
           </Link>
-          <a href="https://www.casperkessels.com/CV_CasperKessels.pdf" className="link hover-nord9 nord1 f5 f4-ns dib mr3 mr4-l">
+          <a href="https://www.casperkessels.com/CV_CasperKessels.pdf " target="_blank" className="link hover-nord9 nord1 f5 f4-ns dib mr3 mr4-l">
             Resume
           </a>
         </div>
